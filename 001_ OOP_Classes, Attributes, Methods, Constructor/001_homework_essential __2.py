0f20a8d422a0e2fda02c0751ec31062b581dc627
VladimirBa <89312397+VladimirBa@users.noreply.github.com>
2021-10-18 16:43:12 +0300

    Update 001_homework_essential __2.py
    Corrected.

--- 001_ OOP_Classes, Attributes, Methods, Constructor/001_homework_essential __2.py
+++ 001_ OOP_Classes, Attributes, Methods, Constructor/001_homework_essential __2.py
@@ -3,10 +3,10 @@ class Temperature:
         self.temperature = temperature
 
     def c_to_f(self):
-        return (self.temperature * 1.8) + 32
+        return (self.temperature * 1.8) + 32.0
 
     def f_to_c(self):
-        return (self.temperature - 32) * 5 / 9
+        return (self.temperature - 32.0) * 5 / 9
 
     @property
     def temperature(self):
@@ -16,16 +16,27 @@ class Temperature:
     def temperature(self, value):
         self._temperature = value
 
+    def exit(self):
+        pass
+
 
 def main():
-    t = float(input("Enter the value of temperature: "))
+    try:
+        t = float(input("Enter the value of temperature: "))
+    except ValueError:
+        print('The value of the temperature has to be number! Enter again.')
+        exit()
+
     unit = input("Enter the scale of the temperature (C - for Celsius; F - for Fahrenheit): ").upper()
     temp = Temperature(t)
 
     if (t < -273.15 and unit == 'C') or (t < -459.4 and unit == 'F'):
-        raise ValueError("Temperature below 'absolute zero' is not possible")
+        print('Temperature below "absolute zero" is not possible')
+        exit()
+
     elif unit != 'C' and unit != 'F':
-        raise ValueError('The temperature scale unit has to be "C" or "F" only.')
+        print('The temperature scale unit has to be "C" or "F" only.')
+        exit()
 
     if unit == 'C':
         print(f"You entered {t} degrees of Celsius that equals {temp.c_to_f():.1f} degrees of Fahrenheit.")
@@ -35,3 +46,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
